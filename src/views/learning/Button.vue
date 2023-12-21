0ef55da72db27eab026442279ccd224779d844b5
mrhbp <partner_sunxiaoyu@saicmotor.com>
2023-12-21 20:30:42 +0800

    test

---
+++ src/views/learning/Button.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>{{ name }}</h1>
+    <h1>{{ age }}</h1>
+    <h1>{{ obj2.age }}</h1>
+    <h1>{{ company }}</h1>
+    <h1>{{ obj.taobao }}</h1>
+    <button @click="btn">this is a button</button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { reactive } from 'vue';
+import {ref} from 'vue'
+
+  const name = ref('abcd')
+  const age = 50
+  const company = 'alibaba'
+  const obj = ref({taobao:'taobao',tianmao:'tianmao'})
+  const obj2 = reactive({age,company})
+
+  function btn(){
+    name.value='ttttttt'
+    obj.value.taobao = 'ttttttt'
+    obj2.age=20
+  }
+
+</script>
